9135235e9b505ba3d4c3c1a717778619dead0bde
Brian Campbell <campbellsoupbs@gmail.com>
2022-06-01 19:30:53 -0400

    Create colorBox() function

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 //variables 
-let height = 10;
-let width = 10;
+let height = 50;
+let width = 50;
 
 //selectors
 let container = document.querySelector('.container');
+
 
 //listeners
 
@@ -13,12 +14,21 @@ let container = document.querySelector('.container');
 function createGrid() {
     for (let i=0; i<(height*width); i++) {
         let newdiv = document.createElement('div');
+        
         newdiv.classList.add('newdiv');
+        newdiv.classList.add(`${i}`);
         newdiv.setAttribute('style', `height:${600/height}px;`);
         newdiv.setAttribute('style', `width:${600/width}px;`);
-
+       
+        newdiv.addEventListener('mouseenter', colorBox);
+        
         container.appendChild(newdiv);
     }
+}
+
+function colorBox(e) {
+    e.target.classList.add('newerclass');
+    console.log(e.target.classList[1]);
 }
 
 createGrid();
